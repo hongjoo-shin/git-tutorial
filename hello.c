90345bedbcb78d9d800015f9fe642b99153c2b2d
hongjoo-shin <frogshj99@naver.com>
2023-03-25 16:36:27 +0900

    Add calc

--- hello.c
+++ hello.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    int a, b;
+
     printf("hello, world!\n");
 
     printf("this is old line\n");
 
+    scanf("%d %d", &a, &b);
+
+    printf("a + b = %d", a + b);
     return 0;
 }
